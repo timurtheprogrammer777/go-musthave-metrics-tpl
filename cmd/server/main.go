8ffe383ee9129cc440b3e09a2d2a5b56be615e48
Timur Enakaev <timur.enakaev.2000@gmail.com>
2025-04-07 19:11:51 +0300

    test2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -21,7 +21,9 @@ func main() {
 	r.Get("/", mainPageMetricNameValue(memStorage))
 
 	r.Get("/value/{type}/{name}", handlers.UpdateHandler(memStorage))
-	http.HandleFunc("/update/", handlers.UpdateHandler(memStorage))
+
+	http.HandleFunc("/update/", handlers.UpdateHandlerPost(memStorage))
+
 	fmt.Printf("Starting server on port %s", PORT)
 	log.Fatal(http.ListenAndServe(PORT, r))
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -46,3 +46,41 @@ func UpdateHandler(storage *storage.MemStorage) http.HandlerFunc {
 		w.WriteHeader(http.StatusOK)
 	}
 }
+
+func UpdateHandlerPost(storage *storage.MemStorage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Only GET method is allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		metricType := chi.URLParam(r, "type")
+		name := chi.URLParam(r, "name")
+		valueStr := chi.URLParam(r, "value")
+
+		switch metricType {
+		case "gauge":
+			value, err := strconv.ParseFloat(valueStr, 64)
+			if err != nil {
+				http.Error(w, "Invalid gauge value", http.StatusBadRequest)
+				return
+			}
+			storage.UpdateGauge(name, value)
+
+		case "counter":
+			value, err := strconv.ParseInt(valueStr, 10, 64)
+			if err != nil {
+				http.Error(w, "Invalid counter value", http.StatusBadRequest)
+				return
+			}
+			storage.UpdateCounter(name, value)
+
+		default:
+			http.Error(w, "Invalid metric type", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Printf("Received metric: Type=%s, Name=%s, Value=%s\n", metricType, name, valueStr)
+		w.WriteHeader(http.StatusOK)
+	}
+}
